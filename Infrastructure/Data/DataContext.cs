441933437c61ba2fecacfc5e31f9550f7d629ed1
vaqef payfoon <vaqef.payfoon@outlook.com>
2023-02-05 18:12:07 +0800

    add-user-entity

---
+++ Infrastructure/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
