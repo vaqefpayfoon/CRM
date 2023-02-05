c5390d498f7920cdbcfd480cec53721feb03d9e7
vaqef payfoon <vaqef.payfoon@outlook.com>
2023-02-05 18:11:14 +0800

    add-user-entity

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+using Domain.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext _data;
+        public UserController(DataContext data)
+        {
+            _data = data;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            return Ok(await _data.Users.ToListAsync());
+        }
+
+        [HttpGet("{id}", Name = "GetById")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _data.Users.FindAsync(id);
+        }
+
+        [HttpGet("AllUsers")]
+        public ActionResult<IEnumerable<AppUser>> AllUsers()
+        {
+            return _data.Users.ToList();
+        }
+
+        [HttpGet("FindUser/{id}", Name = "FindUser")]
+        public ActionResult<AppUser> FindUser(int id)
+        {
+            return _data.Users.Find(id);
+        }
+    }
+}
